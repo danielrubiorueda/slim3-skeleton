609d6336a570f5aa258663cb40f9e8f7ab85d9f0
Daniel Rubio <drubio@cash-converters.es>
2020-11-01 22:02:47 +0100

    no twig debug

--- app/dependencies.php
+++ app/dependencies.php
@@ -14,7 +14,6 @@ $container['view'] = function ($c) {
 
     // Add extensions
     $view->addExtension(new Slim\Views\TwigExtension($c->get('router'), $c->get('request')->getUri()));
-    $view->addExtension(new Twig_Extension_Debug());
 
     return $view;
 };
